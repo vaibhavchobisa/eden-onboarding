9fb3dc355ed40489e3db2b6a7b8e34854055d123
vaibhavchobisa <vaibhav.chobisa@gmail.com>
2023-05-03 15:19:02 +0530

    removed btn click based context state

--- src/context/app.context.jsx
+++ src/context/app.context.jsx
@@ -2,41 +2,24 @@ import { createContext, useState } from "react";
 
 export const defaultFormFields = {
     fullName: '',
-    displayName: ''
+    displayName: '',
 };
 
 // as the actual value you want to access
 export const AppContext = createContext({
-    isFirstClicked: false,
-    setIsFirstClicked: () => { },
-
-    isSecondClicked: false,
-    setIsSecondClicked: () => { },
-
-    isThirdClicked: false,
-    setIsThirdClicked: () => { },
-
     formFields: {},
     setFormFields: () => { },
 });
 
 export const AppProvider = ({ children }) => {
-    const [isFirstClicked, setIsFirstClicked] = useState(false);
-    const [isSecondClicked, setIsSecondClicked] = useState(false);
-    const [isThirdClicked, setIsThirdClicked] = useState(false);
 
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { fullName, displayName } = formFields;
 
     const value = {
-        isFirstClicked,
-        setIsFirstClicked,
-        isSecondClicked,
-        setIsSecondClicked,
-        isThirdClicked,
-        setIsThirdClicked,
         fullName,
         displayName,
+        formFields,
         setFormFields
     };
 
